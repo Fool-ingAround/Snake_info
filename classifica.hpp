32229495b11b9f061186b7e2ead673d862ace453
Craletto <carlocatpan2005@gmail.com>
2025-07-11 06:20:42 +0200

    Update classifica.hpp

--- classifica.hpp
+++ classifica.hpp
@@ -3,31 +3,33 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
-#include <ncurses.h>
+#include <pdcurses.h>
+
 using namespace std;
-constexpr int length=23;   //lunghezza dell'array di nodi
+constexpr int length=24;   //lunghezza dell'array di nodi
 constexpr int maxchar=100;  //lunghezza massima del nome che un giocatore può inserire
 constexpr int start_x=0;  //coordinata x della finestra della classifica
 constexpr int start_y=0;  //coordinata x della finestra della classifica
 constexpr int alt=30;    //altezza della finestra
 constexpr int larg=100;  //larghezza della finestra
 struct nodo {          //struttura nodo che ha al suo intenro il campo nome, quello inserito dal giocatore, e score ovvero il suo punteggio
-    char nome[100];
+    char nome[21];
     int score;
 };
 class classifica {
 protected:
-    WINDOW *leader=newwin(alt, larg, start_x, start_y);     //viene creata la finestra
-    nodo A[length];    //array di nodi
+    static nodo A[length];    //array di nodi
 public:
     classifica();
-    void scambianodo(nodo &x, nodo &y);
-    bool isempty();
-    void bubble_sort();
-    void right_shift(int i);
-    void inserimento(char player[], int punteggio);
-    void scrivi_file();
-    void leggi_file();
-    int stampa_file();
+
+    static void scambianodo(nodo &x, nodo &y);
+    static bool isempty();
+    static void bubble_sort();
+    static void right_shift(int i);
+    static void inserimento(char player[], int punteggio);
+
+    static void scrivi_file();
+    static void leggi_file();
+    static int stampa_file(WINDOW* leader);
 };
 #endif //CLASSIFICA_HPP
